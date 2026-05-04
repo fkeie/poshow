99dd63fceee7aca5719725c6a1cf9918668d3802
fkeie <1280713388@qq.com>
2026-05-04 17:36:03 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,18 @@
-// 定义项目名称，这里叫 poshow
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "poshow"
 include(":app")
